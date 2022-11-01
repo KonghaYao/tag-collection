83750a53c99d1df3b72e43e78c9337fad20f35d0
konghayao <3446798488@qq.com>
2022-11-01 18:13:55 +0800

    feat: 第一次提交

---
+++ README.md
@@ -0,0 +1,10 @@
+
+# Tags 收集整理项目
+
+1. 这个收集整理项目的基础为 使用由 B 站 UP 主 [十二今天也很可爱](https://www.bilibili.com/video/BV1m84y1B7Ny/?p=1&t=285&vd_source=a2ecd44ec8a0a62c70f8b98747f4aa56) 提供的近 4 万个标签。这里特别感谢。
+2. Tags 收集项目脱离于 [魔导绪论项目](https://github.com/KonghaYao/ai-tag), 基于上面的数据集，对部分翻译进行优化，补充部分缺失的 Tag。
+3. Tags 收集项目定时会将数据上传至 NPM，可以供广大社区开发者使用。
+
+# License 开源证书
+
+GPL 3.0
---
+++ src/swearFilter.mjs
@@ -0,0 +1,43 @@
+/**
+ * 违禁词标注，向 tags 里面注入 r18 标记
+ * node 运行即可
+ *  */ 
+import xlsx from 'xlsx';
+
+
+const createList = () => {
+    const a = xlsx.readFile('./data/swearList.csv');
+    const sheet = a.Sheets['Sheet1'];
+    const json = xlsx.utils.sheet_to_json(sheet);
+    return json.map((i) => i.swear);
+};
+
+// 向 public/tags 自动填写 r18 列
+// 违禁词在 swearList 内填写
+const swearList = createList();
+
+const a = xlsx.readFile('./data/tags.csv', {
+    bookType: 'csv',
+    cellFormula: false,
+    cellHTML: false,
+});
+const sheet = a.Sheets['Sheet1'];
+
+const json = xlsx.utils.sheet_to_json(sheet);
+const list = json.map((i) => {
+    try {
+        if (typeof i.en === 'string') {
+            i.r18 = i.en.split(' ').some((word) => swearList.includes(word)) ? 1 : 0;
+        } else {
+            // 数值类型，不作为屏蔽词
+            i.r18 = 0;
+            console.log(i);
+        }
+    } catch (e) {
+        console.log(e);
+        throw e;
+    }
+    return i;
+});
+a.Sheets['Sheet1'] = xlsx.utils.json_to_sheet(list);
+xlsx.writeFile(a, './data/tags.csv', { bookType: 'csv' });
