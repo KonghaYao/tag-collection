8bbac4d0a42862e9802fd5f0f2951068814e3949
konghayao <3446798488@qq.com>
2022-11-10 10:54:16 +0800

    feat: 更换解析插件

--- src/swearFilter.mjs
+++ src/swearFilter.mjs
@@ -1,43 +1,56 @@
 /**
  * 违禁词标注，向 tags 里面注入 r18 标记
  * node 运行即可
- *  */ 
-import xlsx from 'xlsx';
-
-
-const createList = () => {
-    const a = xlsx.readFile('./data/swearList.csv');
-    const sheet = a.Sheets['Sheet1'];
-    const json = xlsx.utils.sheet_to_json(sheet);
-    return json.map((i) => i.swear);
+ *  */
+import papaparser from "papaparse";
+import fs from "fs";
+const createList = async (path, columns) => {
+    const file = fs.readFileSync(path, "utf-8").replace("\uFEFF", "");
+    return new Promise((res) => {
+        papaparser.parse(file, {
+            header: true,
+            dynamicTyping: true,
+            skipEmptyLines: true,
+            columns,
+            complete(results, file) {
+                console.timeEnd("加载 csv 文件");
+                res(results.data);
+            },
+        });
+    });
 };
 
 // 向 public/tags 自动填写 r18 列
 // 违禁词在 swearList 内填写
-const swearList = createList();
+const swearList = await createList("./data/swearList.csv").then((res) =>
+    res.map((i) => i.swear)
+);
 
-const a = xlsx.readFile('./data/tags.csv', {
-    bookType: 'csv',
-    cellFormula: false,
-    cellHTML: false,
-});
-const sheet = a.Sheets['Sheet1'];
-
-const json = xlsx.utils.sheet_to_json(sheet);
-const list = json.map((i) => {
-    try {
-        if (typeof i.en === 'string') {
-            i.r18 = i.en.split(' ').some((word) => swearList.includes(word)) ? 1 : 0;
-        } else {
-            // 数值类型，不作为屏蔽词
-            i.r18 = 0;
-            console.log(i);
-        }
-    } catch (e) {
-        console.log(e);
-        throw e;
-    }
-    return i;
-});
-a.Sheets['Sheet1'] = xlsx.utils.json_to_sheet(list);
-xlsx.writeFile(a, './data/tags.csv', { bookType: 'csv' });
+await createList("./data/tags.csv")
+    .then((res) =>
+        res.map((i) => {
+            try {
+                if (typeof i.en === "string") {
+                    i.r18 = i.en
+                        .split(" ")
+                        .some((word) => swearList.includes(word))
+                        ? 1
+                        : 0;
+                } else {
+                    // 数值类型，不作为屏蔽词
+                    i.r18 = 0;
+                    console.log(i);
+                }
+            } catch (e) {
+                console.log(e);
+                throw e;
+            }
+            return i;
+        })
+    )
+    .then((list) => {
+        return papaparser.unparse(list);
+    })
+    .then((res) => {
+        fs.writeFileSync("./data/tags.csv", "\uFEFF" + res);
+    });
